9aef8aa7dd0f8e59af914d60b800f888a65eaba9
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2024-04-14 20:14:16 +0545

    update: added @ in place of ./ for easy understang and usage

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,5 @@
-import express from 'express';
+import { App } from "@/app";
 
-const app = express();
+const app = new App();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+app.listen();

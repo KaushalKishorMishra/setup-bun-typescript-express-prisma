cd30faea98738f878bc25448765465cd7977e32a
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2024-04-14 20:12:31 +0545

    update: removed the console.log for listening and adding log.info

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import express, { Application } from "express";
+import { log } from "@/utils/logger";
 
 export class App {
-  public app: Application = express();
-    
+  public app: Application;
+  public port: number | string;
+
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+  }
+
+  public listen() {
+    try {
+      this.app.listen(this.port, () => {
+        log.info(`🚀 App listening on the port ${this.port}`);
+      });
+    } catch (err) {
+      log.error(err);
+      console.log(err);
+    }
+  }
 }

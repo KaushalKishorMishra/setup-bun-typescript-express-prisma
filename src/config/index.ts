38ae1e440fe32e02543dffaa03b99145d0718ed2
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2024-04-18 16:03:08 +0545

    modified: extracted image_upload_path and file_upload_path from the env file to use it in another file

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,3 +5,5 @@ config({ path: ".env" });
 export const CREDENTIALS = process.env.CREDENTIALS === "true";
 
 export const { PORT, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
+
+export const { IMAGE_UPLOAD_PATH, FILE_UPLOAD_PATH } = process.env;

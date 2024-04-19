74d46bc6d974211c87a445f7cd4feb086d667cc4
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2024-04-19 17:05:52 +0545

    add: new folder that holds all the repositories for the interaction with database

---
+++ src/repository/users.repository.ts
@@ -0,0 +1,36 @@
+import { User } from "@interfaces/user.interface";
+import { prisma } from "@database";
+
+export class UserRepository {
+    static get_users = async (): Promise<User[]> => {
+        return await prisma.user.findMany();
+    }
+
+    static get_user_by_id = async (id: string): Promise<User> => {
+        return await prisma.user.findUnique({ where: { id } });
+    }
+    static get_user_by_email = async (email: string): Promise<User> => {
+        return await prisma.user.findUnique({ where: { email } });
+    }
+
+    static create_user = async (data: User): Promise<User> => {
+        return await prisma.user.create({ data: data });
+    }
+
+    static update_user = async (id: string, data: User): Promise<User> => {
+        return await prisma.user.update({ where: { id }, data });
+    }
+
+    static soft_delete_user = async (id: string): Promise<User> => {
+        return await prisma.user.update({ where: { id }, data: { deletedAt: new Date() } });
+    }
+
+    static restore_user = async (id: string): Promise<User> => {
+        return await prisma.user.update({ where: { id }, data: { deletedAt: null } });
+    }
+
+    static delete_user = async (id: string): Promise<User> => {
+        return await prisma.user.delete({ where: { id } });
+    }
+
+}

691da67c4196afb0e7833aa5202fbc213f94574d
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2024-06-11 15:09:21 +0545

    add: auth for login and logout for user

--- src/auth/user.auth.ts
+++ src/auth/user.auth.ts
@@ -4,6 +4,10 @@ import { log } from "@utils/logger.utils";
 import { Bcrypt } from "@utils/bcrypt.utils";
 import * as jwt from "jsonwebtoken";
 import { UserRepository } from "@repository/users.repository";
+import { Payload } from "@interfaces/payload.interface";
+import { JWT } from "@services/jwt.service";
+import { User } from "@prisma/client";
+import { TokenType } from "@enums/token.enum";
 
 
 export class UserAuthentication {
@@ -23,10 +27,42 @@ export class UserAuthentication {
                 throw new HttpError(401, "invalid credentials");
             }
 
-            // const token = await jwt.sign({});
+            const payload: Payload = {
+                user_id: user.id,
+                email: user.email,
+                user_type: user.role,
+                type: TokenType.LOGIN,
+            }
 
-            res.status(200).json({ status: 200, data: {  }, message: "login successful" });
+            const jwt_token: string = await JWT.sign_jwt(payload, "10d");
+
+            res.status(200).json({ status: 200, data: { user }, message: "login successful", jwt: jwt_token });
         } catch (error) {
+            log.error(error);
+            next(error);
+        }
+    }
+
+    static logout = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const token = req.headers.authorization?.split(" ")[1];
+            
+            if (!token) {
+                throw new HttpError(401, "unauthorized");
+            }
+            const payload: Payload = await JWT.verify_jwt(token);
+
+            const user_id = payload.user_id;
+
+            const user = await UserRepository.get_user_by_id(user_id);
+
+            if (!user) {
+                throw new HttpError(404, "user not found");
+            }
+            
+            res.status(200).json({ status: 200, message: "logout successful" });
+        }
+        catch (error) {
             log.error(error);
             next(error);
         }
